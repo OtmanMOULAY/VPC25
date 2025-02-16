63ab1ef1f6666792f7d07e39ffb796224f5384e6
Othman_moulay <otmanaitmoulay@gmail.com>
2025-02-16 03:18:06 +0100

    first commit

--- model.py
+++ model.py
@@ -1,37 +1,75 @@
-#############################################################################
-# YOUR ANONYMIZATION MODEL
-# ---------------------
-# Should be implemented in the 'anonymize' function
-# !! *DO NOT MODIFY THE NAME OF THE FUNCTION* !!
-#
-# If you trained a machine learning model you can store your parameters in any format you want (npy, h5, json, yaml, ...)
-# <!> *SAVE YOUR PARAMETERS IN THE parameters/ DICRECTORY* <!>
-############################################################################
+import librosa
+import numpy as np
+import soundfile as sf
+import IPython.display as idisplay
+import matplotlib.pyplot as plt
 
-def anonymize(input_audio_path): # <!> DO NOT ADD ANY OTHER ARGUMENTS <!>
-    """
-    anonymization algorithm
-
-    Parameters
-    ----------
-    input_audio_path : str
-        path to the source audio file in one ".wav" format.
-
-    Returns
-    -------
-    audio : numpy.ndarray, shape (samples,), dtype=np.float32
-        The anonymized audio signal as a 1D NumPy array of type `np.float32`, 
-        which ensures compatibility with `soundfile.write()`.
-    sr : int
-        The sample rate of the processed audio.
-    """
-
-    # Read the source audio file
-
-    # Apply your anonymization algorithm
+def interpolate_time(idxs: np.ndarray, arr):
+    start = np.floor(idxs).astype(int) 
+    frac = (idxs - start)[None, None, :]
     
-    # Output:
-    audio = ...
-    sr = ...
+    # Avoid out-of-bounds indexing
+    start = np.clip(start, 0, arr.shape[2] - 2)
     
+    shifted_arr = np.concatenate((arr[:, :, 1:], np.zeros((arr.shape[0], arr.shape[1], 1))), axis=2)
+    return arr[:, :, start] * (1 - frac) + shifted_arr[:, :, start] * frac
+
+def round_interpolate_time(idxs: np.ndarray, arr):
+    idxs = np.clip((idxs + 0.5).astype(int), 0, arr.shape[2] - 1)  # Prevent out-of-bounds
+    return arr[:, :, idxs]
+
+pitch_shift = -1.5
+n_fft = 4096
+hop_len = 1024
+win_len = 4096
+
+def anonymize(input_audio_path): 
+    waveform, sr = librosa.load(input_audio_path, sr=None, mono=False)
+    original_length = waveform.shape[1] if waveform.ndim > 1 else waveform.shape[0]
+
+    if waveform.ndim == 1:
+        waveform = np.expand_dims(waveform, axis=0)  
+    
+    anls_stft = librosa.stft(waveform, n_fft=n_fft, hop_length=hop_len, win_length=win_len)
+    channels, n_anls_freqs, n_anls_frames = anls_stft.shape
+    
+    scaling = 2 ** (pitch_shift / 12)
+    n_synth_frames = np.floor(n_anls_frames * scaling).astype(int)
+    synth_frames = np.arange(n_synth_frames)
+    og_idxs = np.minimum(synth_frames / scaling, n_anls_frames - 1)
+    
+    mags = np.abs(anls_stft)
+    phases = np.angle(anls_stft)
+    phase_diffs = np.mod(phases - np.concatenate((np.zeros((channels, n_anls_freqs, 1)), phases[:, :, :-1]), axis=2), np.pi * 2)
+    
+    shifted_mags = interpolate_time(og_idxs, mags)
+    shifted_phase_diffs = interpolate_time(og_idxs, phase_diffs)
+    unshifted_phases = round_interpolate_time(og_idxs, phases)
+    
+    shifted_phases = np.zeros((channels, n_anls_freqs, n_synth_frames))
+    shifted_phases[:, :, 0] = shifted_phase_diffs[:, :, 0]
+    epsilon = 1e-8  
+    for t in range(1, n_synth_frames):
+        time_phases = shifted_phases[:, :, t - 1] + shifted_phase_diffs[:, :, t]
+        freq_phases = unshifted_phases[:, :, t]
+        transient = (shifted_mags[:, :, t] - shifted_mags[:, :, t - 1]) / (np.maximum(shifted_mags[:, :, t] + shifted_mags[:, :, t - 1], epsilon))
+        transient[transient < 0.5] = 0
+        transient[transient >= 0.5] = 1
+        shifted_phases[:, :, t] = np.mod(freq_phases * transient + time_phases * (1 - transient), np.pi * 2)
+    
+    synth_stft = shifted_mags * np.exp(shifted_phases * 1j)
+    new_waveform = librosa.istft(synth_stft, hop_length=hop_len, win_length=win_len, n_fft=n_fft)
+
+   
+    new_waveform = librosa.resample(new_waveform, orig_sr=len(new_waveform.T), target_sr=original_length)
+
+   
+    output_audio_path = "output_audio.wav"
+    sf.write(output_audio_path, new_waveform.T, sr)
+
+    audio = new_waveform.T
+    sr  = sr
+
     return audio, sr
+
+print(anonymize("evaluation_data/Trial/speaker10/67-150225-03.wav"))
